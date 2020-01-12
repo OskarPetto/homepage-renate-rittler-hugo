fa9fc27ac0ae3c36a49f249df44f0c5bfea26180
Oskar Petto <oskar.petto@hotmail.com>
2020-01-12 22:54:27 +0100

    add lazy loading

--- themes/artist/static/js/modal.js
+++ themes/artist/static/js/modal.js
@@ -1,33 +1,35 @@
 
 function scaleModalToImage() {
-
-    if (window.innerWidth < 1200) {
-        return;
-    }
     
     const image = $("#imageModal").find(".active").find("img")[0];
 
     const ratio = image.naturalWidth / image.naturalHeight;
 
     // for modal footer
-    const maxModalHeight = window.innerHeight - 220;
+    const maxModalHeight = window.innerHeight - 210;
 
     const maxHeight = Math.min(image.naturalHeight, maxModalHeight);
 
     // for padding in modal
-    const maxWidth = maxHeight * ratio + 34; 
+    const maxWidth = Math.min(window.innerWidth - 34,  maxHeight * ratio + 34); 
 
     $("#imageModal").find(".modal-dialog").css({ "max-width": maxWidth});
+}
+
+function setImageSrc(e) {
+    let image = $(e.relatedTarget).find('img[data-src]');
+    image.attr('src', image.data('src'));
+    image.removeAttr('data-src');
 }
 
 // scale modal on show
 $("#imageModal").on('show.bs.modal', function (e) {
     
-    scaleModalToImage();
+    setImageSrc(e);
   })
 
 // scale modal on next or prev click
 $("#imageCarousel").on('slid.bs.carousel', function (e) {
     
-    scaleModalToImage();
+    setImageSrc(e);
 })
