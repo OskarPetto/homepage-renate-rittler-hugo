812dc91952bd764b504fc6a5764146a28c9211c6
Oskar Petto <oskar.petto@hotmail.com>
2020-01-12 09:39:56 +0100

    add modal with carousel

--- themes/artist/static/js/list.js
+++
@@ -1,5 +0,0 @@
-
-$(".modal-link").on("click", function() {
-    $('#imagepreview').attr('src', $('.preview-image').attr('src')); // here asign the image to the modal when the user click the enlarge link
-    $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
- });
