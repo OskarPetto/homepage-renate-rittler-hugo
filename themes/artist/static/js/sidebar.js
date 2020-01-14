19b46df1c5edf53f8c19bffce81bbe3f71f02135
Oskar Petto <oskar.petto@hotmail.com>
2020-01-14 15:22:50 +0100

    use different sidebar images

--- themes/artist/static/js/sidebar.js
+++ themes/artist/static/js/sidebar.js
@@ -1,5 +1,5 @@
 // For smooth scrolling on mobile because 100vh is not constant 
 if (window.innerWidth < 576) {
     const viewportHeight = $('.sidebar').innerHeight();
-    $('.sidebar').css({ 'min-height': viewportHeight * 0.8 });
+    $('.sidebar').css({ 'height': viewportHeight * 0.8 });
 }
